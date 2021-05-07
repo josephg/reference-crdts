7ea4d6791f71989e9f6389f4b5a2583cdf047dc2
Seph Gentle <me@josephg.com>
2021-05-07 16:27:31 +1000

    Small cleanups. Made ID a tuple. Added randomized order variant testing

--- test.ts
+++ test.ts
@@ -1,6 +1,10 @@
 import assert from 'assert/strict'
+import seed from 'seed-random'
+
 
 type Id = [agent: string, seq: number]
+type Version = Record<string, number> // Last seen seq for each agent.
+
 // type Id = {
 //   agent: string,
 //   seq: number,
@@ -20,8 +24,10 @@ type Item<T> = {
 
 interface Doc<T = string> {
   content: Item<T>[]
-  version: Record<string, number> // agent => last seen seq.
+  version: Version // agent => last seen seq.
 }
+
+const newDoc = <T>(): Doc<T> => ({content: [], version: {}})
 
 const idEq = (a: Id | null, b: Id | null): boolean => (
   a === b || (a != null && b != null && a[0] === b[0] && a[1] === b[1])
@@ -124,6 +130,59 @@ const getArray = <T>(doc: Doc<T>): T[] => (
 /// TESTS
 
 ;(() => { // Separate scope for namespace protection.
+  const random = seed('ax')
+  const randInt = (n: number) => Math.floor(random() * n)
+  const randBool = (weight: number = 0.5) => random() < weight
+
+  const isInVersion = (id: Id | null, version: Version) => {
+    if (id == null) return true
+    const seq = version[id[0]]
+    return seq != null && seq >= id[1]
+  }
+
+  const integrateFuzzOnce = <T>(ops: Item<T>[], expectedResult: T[]): number => {
+    let variants = 1
+    const doc = newDoc()
+
+    // Scan ops looking for candidates to integrate
+    for (let numIntegrated = 0; numIntegrated < ops.length; numIntegrated++) {
+      const candidates = []
+      for (const op of ops) {
+        // We need op.id to not be in doc.versions, but originLeft and originRight to be in.
+        // We're also inserting each item from each agent in sequence.
+        if (!isInVersion(op.id, doc.version)
+            && (op.id[1] === 0 || isInVersion([op.id[0], op.id[1] - 1], doc.version))
+            && isInVersion(op.originLeft, doc.version)
+            && isInVersion(op.originRight, doc.version)) {
+          candidates.push(op)
+
+          // console.log(op.id, doc.version, isInVersion(op.id, doc.version))
+        }
+      }
+      assert(candidates.length > 0)
+      variants *= candidates.length
+      // console.log('doc version', doc.version, 'candidates', candidates)
+
+      // Pick one
+      const op = candidates[randInt(candidates.length)]
+      // console.log(op, doc.version)
+      integrate(doc, op)
+    }
+
+    assert.deepStrictEqual(getArray(doc), expectedResult)
+    // console.log(variants)
+    return variants // Rough guess at the number of orderings
+  }
+
+
+  const integrateFuzz = <T>(ops: Item<T>[], expectedResult: T[]) => {
+    // Integrate the passed items a bunch of times, in different orders.
+    let variants = integrateFuzzOnce(ops, expectedResult)
+    for (let i = 1; i < Math.min(variants * 3, 100); i++) {
+      let newVariants = integrateFuzzOnce(ops, expectedResult)
+      variants = Math.max(variants, newVariants)
+    }
+  }
 
   const test = (fn: () => void) => {
     process.stdout.write(`running ${fn.name} ...`)
@@ -132,106 +191,64 @@ const getArray = <T>(doc: Doc<T>): T[] => (
   }
 
   const smoke = () => {
-    const doc: Doc = {content: [], version: {}}
+    const doc = newDoc()
     integrate(doc, makeItem('a', ['A', 0], null, null))
     integrate(doc, makeItem('b', ['A', 1], ['A', 0], null))
 
-    // console.log(doc.content.map(x => x.content))
-    // console.log(doc.content)
     assert.deepEqual(getArray(doc), ['a', 'b'])
   }
 
   const concurrentAvsB = () => {
     const a = makeItem('a', 'A', null, null)
     const b = makeItem('b', 'B', null, null)
-
-    const doc: Doc = {content: [], version: {}}
-    integrate(doc, a)
-    integrate(doc, b)
-    assert.deepEqual(getArray(doc), ['a', 'b'])
-
-    const doc2: Doc = {content: [], version: {}}
-    integrate(doc2, b)
-    integrate(doc2, a)
-    assert.deepEqual(getArray(doc2), ['a', 'b'])
+    integrateFuzz([a, b], ['a', 'b'])
   }
 
   const interleavingForward = () => {
-    const as = [
+    const ops = [
       makeItem('a', ['A', 0], null, null),
       makeItem('a', ['A', 1], ['A', 0], null),
       makeItem('a', ['A', 2], ['A', 1], null),
-    ]
-    const bs = [
+
       makeItem('b', ['B', 0], null, null),
       makeItem('b', ['B', 1], ['B', 0], null),
       makeItem('b', ['B', 2], ['B', 1], null),
     ]
 
-    const doc: Doc = {content: [], version: {}}
-    as.forEach(item => integrate(doc, item))
-    bs.forEach(item => integrate(doc, item))
-    assert.deepEqual(getArray(doc), ['a', 'a', 'a', 'b', 'b', 'b'])
-
-    // And with a different interleaving. It'd be better to play with more variants here.
-    const doc2: Doc = {content: [], version: {}}
-    bs.forEach(item => integrate(doc2, item))
-    as.forEach(item => integrate(doc2, item))
-    assert.deepEqual(getArray(doc2), ['a', 'a', 'a', 'b', 'b', 'b'])
+    integrateFuzz(ops, ['a', 'a', 'a', 'b', 'b', 'b'])
   }
 
   const interleavingBackward = () => {
-    const as = [
+    const ops = [
       makeItem('a', ['A', 0], null, null),
       makeItem('a', ['A', 1], null, ['A', 0]),
       makeItem('a', ['A', 2], null, ['A', 1]),
-    ]
-    const bs = [
+
       makeItem('b', ['B', 0], null, null),
       makeItem('b', ['B', 1], null, ['B', 0]),
       makeItem('b', ['B', 2], null, ['B', 1]),
     ]
 
-    const doc: Doc = {content: [], version: {}}
-    as.forEach(item => integrate(doc, item))
-    bs.forEach(item => integrate(doc, item))
-    assert.deepEqual(getArray(doc), ['a', 'a', 'a', 'b', 'b', 'b'])
-
-    // And with a different interleaving. It'd be better to play with more variants here.
-    const doc2: Doc = {content: [], version: {}}
-    bs.forEach(item => integrate(doc2, item))
-    as.forEach(item => integrate(doc2, item))
-    assert.deepEqual(getArray(doc2), ['a', 'a', 'a', 'b', 'b', 'b'])
+    integrateFuzz(ops, ['a', 'a', 'a', 'b', 'b', 'b'])
   }
 
   const withTails = () => {
-    const as = [
+    const ops = [
       makeItem('a', ['A', 0], null, null),
       makeItem('a0', ['A', 1], null, ['A', 0]), // left
       makeItem('a1', ['A', 2], ['A', 0], null), // right
-    ]
-    const bs = [
+
       makeItem('b', ['B', 0], null, null),
       makeItem('b0', ['B', 1], null, ['B', 0]), // left
       makeItem('b1', ['B', 2], ['B', 0], null), // right
     ]
 
-    const doc: Doc = {content: [], version: {}}
-    as.forEach(item => integrate(doc, item))
-    bs.forEach(item => integrate(doc, item))
-    assert.deepEqual(getArray(doc), ['a0', 'a', 'a1', 'b0', 'b', 'b1'])
-
-    // And with a different interleaving.
-    const doc2: Doc = {content: [], version: {}}
-    bs.forEach(item => integrate(doc2, item))
-    as.forEach(item => integrate(doc2, item))
-    assert.deepEqual(getArray(doc2), ['a0', 'a', 'a1', 'b0', 'b', 'b1'])
+    integrateFuzz(ops, ['a0', 'a', 'a1', 'b0', 'b', 'b1'])
   }
 
   const localVsConcurrent = () => {
     // Check what happens when a top level concurrent change interacts
     // with a more localised change. (C vs D)
-    const doc: Doc = {content: [], version: {}}
     const a = makeItem('a', 'A', null, null)
     const c = makeItem('c', 'C', null, null)
 
@@ -239,11 +256,8 @@ const getArray = <T>(doc: Doc<T>): T[] => (
     const b = makeItem('b', 'B', null, null) // Concurrent with a and c
     const d = makeItem('d', 'D', ['A', 0], ['C', 0]) // in between a and c
 
-    integrate(doc, a)
-    integrate(doc, c)
-    integrate(doc, b)
-    integrate(doc, d)
-    assert.deepEqual(getArray(doc), ['a', 'd', 'b', 'c'])
+    // [a, b, d, c] would also be acceptable.
+    integrateFuzz([a, b, c, d], ['a', 'd', 'b', 'c'])
   }
 
   const tests = [
