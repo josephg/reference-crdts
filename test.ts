68ab6b9f32367d1d1b72f8d7ebd62b85e118ce7f
Seph Gentle <me@josephg.com>
2021-05-08 20:53:19 +1000

    Fixed a bug in yjs implementation - removed unnecessary case. Added small index hints & lookups, which improve performance by 2-4x.

--- test.ts
+++ test.ts
@@ -7,7 +7,7 @@ type Id = [agent: string, seq: number]
 type Version = Record<string, number> // Last seen seq for each agent.
 
 type Algorithm = {
-  integrate: <T>(doc: Doc<T>, newItem: Item<T>) => void
+  integrate: <T>(doc: Doc<T>, newItem: Item<T>, idx_hint?: number) => void
   ignoreTests?: string[]
 }
 
@@ -44,9 +44,31 @@ const idEq = (a: Id | null, b: Id | null): boolean => (
   a === b || (a != null && b != null && a[0] === b[0] && a[1] === b[1])
 )
 
-const findItem = <T>(doc: Doc<T>, needle: Id | null): number => {
+let hits = 0
+let misses = 0
+
+// idx_hint is a small optimization so when we know the general area of
+// an item, we search nearby instead of just scanning the whole document.
+const findItem = <T>(doc: Doc<T>, needle: Id | null, idx_hint: number = -1): number => {
   if (needle == null) return -1
   else {
+    // This little optimization *halves* the time to run the editing trace benchmarks.
+    if (idx_hint >= 0 && idx_hint < doc.content.length) {
+      if (idEq(doc.content[idx_hint].id, needle)) {
+        hits++
+        return idx_hint
+      }
+      // Try nearby.
+      const RANGE = 10
+      for (let i = idx_hint < RANGE ? 0 : idx_hint - RANGE; i < doc.content.length && i < idx_hint + RANGE; i++) {
+        if (idEq(doc.content[i].id, needle)) {
+          hits++
+          return i
+        }
+      }
+    }
+
+    misses++
     const idx = doc.content.findIndex(({id}) => idEq(id, needle))
     if (idx < 0) throw Error('Could not find item') // Could use a ternary if not for this!
     return idx
@@ -55,14 +77,14 @@ const findItem = <T>(doc: Doc<T>, needle: Id | null): number => {
 
 // This isn't actually yjs - I've done a few tweaks to make the CRDT
 // puzzles below work better.
-const integrateYjsMod = <T>(doc: Doc<T>, newItem: Item<T>) => {
+const integrateYjsMod = <T>(doc: Doc<T>, newItem: Item<T>, idx_hint: number = -1) => {
   const lastSeen = doc.version[newItem.id[0]] ?? -1
   if (newItem.id[1] !== lastSeen + 1) throw Error('Operations out of order')
   doc.version[newItem.id[0]] = newItem.id[1]
 
-  let left = findItem(doc, newItem.originLeft)
+  let left = findItem(doc, newItem.originLeft, idx_hint - 1)
   let destIdx = left + 1
-  let right = newItem.originRight == null ? doc.content.length : findItem(doc, newItem.originRight)
+  let right = newItem.originRight == null ? doc.content.length : findItem(doc, newItem.originRight, idx_hint)
   let conflictStart = -1
 
   const startConflict = (i: number) => conflictStart = i
@@ -76,14 +98,12 @@ const integrateYjsMod = <T>(doc: Doc<T>, newItem: Item<T>) => {
 
     let o = doc.content[i]
 
-    let oleft = findItem(doc, o.originLeft)
-    let oright = o.originRight == null ? doc.content.length : findItem(doc, o.originRight)
+    let oleft = findItem(doc, o.originLeft, idx_hint - 1)
+    let oright = o.originRight == null ? doc.content.length : findItem(doc, o.originRight, idx_hint)
 
     // Ok now we implement the punnet square of behaviour
     if (oleft < left) {
       // Top row. Insert, insert, arbitrary (insert)
-      // I'm not sure a conflict is a problem here, but I can't generate one with my tests!
-      if (conflictStart >= 0) throw Error('Unexpected conflict')
       break
     } else if (oleft === left) {
       // Middle row.
@@ -111,7 +131,7 @@ const integrateYjsMod = <T>(doc: Doc<T>, newItem: Item<T>) => {
   doc.length += 1
 }
 
-const integrateAM = <T>(doc: Doc<T>, newItem: Item<T>) => {
+const integrateAM = <T>(doc: Doc<T>, newItem: Item<T>, idx_hint: number = -1) => {
   const {id} = newItem
   assert(newItem.seq >= 0)
 
@@ -119,13 +139,18 @@ const integrateAM = <T>(doc: Doc<T>, newItem: Item<T>) => {
   if (id[1] !== lastSeen + 1) throw Error('Operations out of order')
   doc.version[id[0]] = id[1]
 
-  let parent = findItem(doc, newItem.originLeft)
+  let parent = findItem(doc, newItem.originLeft, idx_hint - 1)
   let destIdx = parent + 1
 
   // Scan for the insert location. Stop if we reach the end of the document
   for (; destIdx < doc.content.length; destIdx++) {
     let o = doc.content[destIdx]
-    let oparent = findItem(doc, o.originLeft)
+
+    // Optimization: This call halves the speed of this automerge
+    // implementation. Its only needed to see if o.originLeft has been
+    // visited in this loop, which we could calculate much more
+    // efficiently.
+    let oparent = findItem(doc, o.originLeft, idx_hint - 1)
 
     // Ok now we implement the punnet square of behaviour
     if (oparent < parent) {
@@ -191,7 +216,7 @@ const localInsert = <T>(alg: Algorithm, doc: Doc<T>, agent: string, pos: number,
     originLeft: doc.content[i - 1]?.id ?? null,
     originRight: doc.content[i]?.id ?? null, // Only for yjs
     seq: doc.maxSeq + 1, // Only for AM.
-  })
+  }, i)
 }
 
 const localDelete = <T>(doc: Doc<T>, agent: string, pos: number): void => {
@@ -419,47 +444,50 @@ const runTests = (alg: Algorithm) => { // Separate scope for namespace protectio
 
   const fuzzMultidoc = () => {
     const agents = ['A', 'B', 'C']
-    const docs = new Array(3).fill(null).map((_, i) => {
-      const doc: Doc<number> & {agent: string} = newDoc() as any
-      doc.agent = agents[i]
-      return doc
-    })
+    for (let j = 0; j < 10; j++) {
+      process.stdout.write('.')
+      const docs = new Array(3).fill(null).map((_, i) => {
+        const doc: Doc<number> & {agent: string} = newDoc() as any
+        doc.agent = agents[i]
+        return doc
+      })
 
-    const randDoc = () => docs[randInt(docs.length)]
+      const randDoc = () => docs[randInt(docs.length)]
 
-    let nextItem = 0
-    // console.log(docs)
-    for (let i = 0; i < 1000; i++) {
-      // console.log(i)
-      if (i % 100 === 0) console.log(i)
+      let nextItem = 0
+      // console.log(docs)
+      for (let i = 0; i < 1000; i++) {
+        // console.log(i)
+        // if (i % 100 === 0) console.log(i)
 
-      // Generate some random operations
-      for (let j = 0; j < 3; j++) {
-        const doc = randDoc()
+        // Generate some random operations
+        for (let j = 0; j < 3; j++) {
+          const doc = randDoc()
 
-        // if (doc.length === 0 || randBool(0.5)) {
-        if (true) {
-          // insert
-          const pos = randInt(doc.length + 1)
-          const content = ++nextItem
-          // console.log('insert', agent, pos, content)
-          localInsert(alg, doc, doc.agent, pos, content)
-        } else {
-          // Delete - disabled for now because mergeInto doesn't support deletes
-          const pos = randInt(doc.length)
-          // console.log('delete', pos)
-          localDelete(doc, doc.agent, pos)
+          // if (doc.length === 0 || randBool(0.5)) {
+          if (true) {
+            // insert
+            const pos = randInt(doc.length + 1)
+            const content = ++nextItem
+            // console.log('insert', agent, pos, content)
+            localInsert(alg, doc, doc.agent, pos, content)
+          } else {
+            // Delete - disabled for now because mergeInto doesn't support deletes
+            const pos = randInt(doc.length)
+            // console.log('delete', pos)
+            localDelete(doc, doc.agent, pos)
+          }
         }
-      }
 
-      // Pick a pair of documents and merge them
-      const a = randDoc()
-      const b = randDoc()
-      if (a !== b) {
-        // console.log('merging', a.id, b.id, a.content, b.content)
-        mergeInto(alg, a, b)
-        mergeInto(alg, b, a)
-        assert.deepStrictEqual(getArray(a), getArray(b))
+        // Pick a pair of documents and merge them
+        const a = randDoc()
+        const b = randDoc()
+        if (a !== b) {
+          // console.log('merging', a.id, b.id, a.content, b.content)
+          mergeInto(alg, a, b)
+          mergeInto(alg, b, a)
+          assert.deepStrictEqual(getArray(a), getArray(b))
+        }
       }
     }
   }
@@ -478,6 +506,7 @@ const runTests = (alg: Algorithm) => { // Separate scope for namespace protectio
   ]
   tests.forEach(test)
   // fuzzSequential()
+  // fuzzMultidoc()
 }
 
 const yjsMod: Algorithm = {
@@ -520,9 +549,7 @@ const bench = (alg: Algorithm) => {
   console.timeEnd(filename)
 }
 
-// console.time('seph yjs impl')
-// bench(sephcrdt)
-// console.timeEnd('seph yjs impl')
-// console.time('automerge')
-bench(automerge)
-// console.timeEnd('automerge')
+// bench(yjsMod)
+// bench(automerge)
+
+// console.log('hits', hits, 'misses', misses)
